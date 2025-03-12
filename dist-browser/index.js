41f638ddfc3addba8b3b75e5a66ae11e2bba07f3
Lorenzo Antonio Rojo Garces <lorenzodev2020@gmail.com>
2025-03-12 13:27:40 -0500

    Initial commit with files library

---
+++ dist-browser/index.js
@@ -0,0 +1,28 @@
+/**
+ * Add two numbers
+ * @param a Number one
+ * @param b Number two
+ * @return Result
+ */
+export var add = function (a, b) { return a + b; };
+/**
+ * sub two numbers
+ * @param a Number one
+ * @param b Number two
+ * @return Result
+ */
+export var sub = function (a, b) { return a - b; };
+/**
+ * multiply two numbers
+ * @param a Number one
+ * @param b Number two
+ * @return Result
+ */
+export var multiply = function (a, b) { return a * b; };
+/**
+ * divide two numbers
+ * @param a Number one
+ * @param b Number two
+ * @return Result
+ */
+export var divide = function (a, b) { return a * b; };
---
+++ src/index.ts
@@ -0,0 +1,28 @@
+/**
+ * Add two numbers
+ * @param a Number one
+ * @param b Number two
+ * @return Result
+ */
+export const add = (a: number  ,b: number):number => a+b;
+/**
+ * sub two numbers
+ * @param a Number one
+ * @param b Number two
+ * @return Result
+ */
+export const sub = (a: number, b: number): number => a -b;
+/**
+ * multiply two numbers
+ * @param a Number one
+ * @param b Number two
+ * @return Result
+ */
+export const multiply = (a: number, b: number): number => a * b;
+/**
+ * divide two numbers
+ * @param a Number one
+ * @param b Number two
+ * @return Result
+ */
+export const divide = (a: number, b: number): number => a * b;
